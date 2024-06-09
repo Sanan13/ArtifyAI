d304f765bc262f47121a835a282ea08cc891e0ea
saiyed10 <saiyedsanan10@gmail.com>
2024-06-10 00:37:38 +0530

    logo refactor

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -12,7 +12,7 @@ const MobileNav = () => {
     return (
         <header className="header">
             <Link href="/" className="flex items-center gap-2 md:py-2">
-                <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+                <Image src="/assets/images/my-logo.png" alt="logo" width={180} height={28} />
             </Link>
             <nav className="flex gap-2">
                 <SignedIn>
@@ -21,7 +21,7 @@ const MobileNav = () => {
                         <SheetTrigger><Image src="/assets/icons/menu.svg" alt="menu" width={32} height={32} className="cursor-pointer" /></SheetTrigger>
                         <SheetContent className="sheet-content sm:w-64">
                             <>
-                                <Image src="/assets/images/logo-text.svg" alt="logo" width={152} height={23} />
+                                <Image src="/assets/images/my-logo.png" alt="logo" width={152} height={23} />
                                 <ul className='header-nav_elements'>
                                     {navLinks.map((link, idex) => {
                                         const isActive = link.route === pathname
